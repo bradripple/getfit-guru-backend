47ba573e4eb66b5585c9162f7731865dd0531cfc
bradripple <bradripple@gmail.com>
2021-09-28 18:20:51 -0600

    Update: created post route for exercise submit

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -1,27 +1,44 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
-const passport = require('passport');
+const passport = require("passport");
 
-const { Exercise } = require('../models');
+const { Exercise } = require("../models");
 
-router.get('/', async ( req, res ) => {
-    try {
-        let allExercises = await Exercise.find({});
+router.get("/", async (req, res) => {
+  try {
+    let allExercises = await Exercise.find({});
 
-        res.status(200).json({
-            exercises: allExercises
-        })
-        
-    } catch (error) {
-        console.log('exercises page', error);
-        res.status(500).json({
-            message: 'There was an error. Please try again.'
-        })
-    }
-})
+    res.status(200).json({
+      exercises: allExercises,
+    });
+  } catch (error) {
+    console.log("exercises page", error);
+    res.status(500).json({
+      message: "There was an error. Please try again.",
+    });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    let newExercise = await Exercise.create({
+      type: req.body.type,
+      muscleGroup: req.body.muscleGroup,
+      name: req.body.name,
+      durationGoal: req.body.durationGoal,
+      weightGoal: req.body.weightGoal,
+      repsGoal: req.body.repsGoal,
+      setsGoal: req.body.setsGoal,
+      distanceGoal: req.body.distanceGoal
+    });
+
+    console.log(newExercise);
+    res.json(newExercise);
+  } catch (error) {}
+});
 
 module.exports = router;
